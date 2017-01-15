10b631c994292a0decfd976f8f910833fdf77ffe
Yunhyeok Lee <yunhyeoklee@Yunhyeoks-MacBook-Pro.local>
2017-01-15 16:20:10 -0600

    add data bining part of adding list :)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,12 +25,38 @@ angular.module('movieManiaApp')
       description:'This is a book that i want to explain to you ! :)'
     }];
     $scope.movies = movie;
+
+    $scope.newMovieTitle = '';
+    $scope.newMovieDescription = '';
+    $scope.newMovieImage = 'http://post.phinf.naver.net/MjAxNzAxMDVfNDEg/MDAxNDgzNTgyMTkxODcy.Lnh8qTRc2Vm2tEKJC8hgtnzAS5uR2i2M-y86m32hnr4g.Ql7V0uYF8yrIakf_L-PjqluApwX7QnyuQdgLCKe5kIYg.JPEG/%EB%9D%BC%EC%98%A4%EC%96%B4.jpg?type=w1200';
+
+    $scope.validateTitle = function() {
+      if($scope.newMoiveTitle.length > 0) {
+        console.debug($scope.newMoiveTitle);
+      }
+      else {
+        window.alert("틀렸엉!! 바꿔");
+      }
+    };
+
+    $scope.addMovie = function() {
+      var movie = {
+        title: $scope.newMoiveTitle,
+        category: $scope.newMovieCategory,
+        image: $scope.newMovieImage,
+        description: $scope.newMovieDescription
+      };
+      $scope.movies.push(movie);
+    };
+
+    $scope.checkCategorySelected = function() {
+      if($scope.newMovieCategory === '') {
+        window.alert('카테고리를 비우면 안되용!');
+      }
+    };
+
+    $scope.checkDescription = function() {
+        console.debug($scope.newMovieDescription);
+    };
   //  $scope.movieImage = movie.image;
   })
-  .controller('FormCtrl', function($scope){
-    console.log('HEHE');
-
-  })
-  .controller('SubCtrl', function($scope){
-    $scope.moviesofSub = $scope.movies;
-  });
